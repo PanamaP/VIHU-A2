31aae830b641aa9772700dcfd4f1d30c803549c3
Elfar Snær <Elfarsnaer@gmail.com>
2024-02-26 18:07:16 +0000

    lint fixing

--- eslint_rules/index.js
+++ eslint_rules/index.js
@@ -5,12 +5,8 @@ const ruleFiles = fs
   .readdirSync(__dirname)
   .filter((file) => file !== "index.js" && !file.endsWith("test.js"));
 
-// Use `require` to dynamically import the rules based on the filenames.
-// This assumes that the rules are CommonJS modules.
-const rules = ruleFiles.reduce((acc, file) => {
-  const ruleName = path.basename(file, ".js");
-  acc[ruleName] = require("./" + file);
-  return acc;
-}, {});
+const rules = Object.fromEntries(
+  ruleFiles.map((file) => [path.basename(file, ".js"), require("./" + file)])
+);
 
 module.exports = { rules };
